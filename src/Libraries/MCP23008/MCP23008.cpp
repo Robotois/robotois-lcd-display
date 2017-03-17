bf496d1c27644e56fe8dc2c1fd6cea85c30adcc7
yovany-lg <yovany@gmail.com>
2017-03-17 18:24:18 +0000

    Release method fixes

--- src/Libraries/MCP23008/MCP23008.cpp
+++ src/Libraries/MCP23008/MCP23008.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   MCP23008.cpp
  * Author: yova
- * 
+ *
  * Created on 27 de mayo de 2016, 07:57 PM
  */
 #include <stdint.h>
@@ -13,18 +13,18 @@
 
 MCP23008::MCP23008(uint8_t _addr) {
     uint8_t result;
-    
+
     if(_addr > 0x07){ // - 3 bits for custom address
         printf("Wrong slave address for the Digital IO Module...\n");
-        return;    
+        return;
     }
-    
+
     slave_addr = MCP23008_ADDRESS | _addr;
-    
+
 //    printf("Slave address... %d\n",slave_addr);
-    
+
     bcm_init();
-    
+
     selectModule();
     // - Read from the MCP23008 device to make sure it is connected
     wBuf[0] = MCP23008_IODIR;
@@ -49,12 +49,12 @@ void MCP23008::selectModule(){
 void MCP23008::setDir(uint8_t _pin, uint8_t dir){
     uint8_t io_dir_mask;
     selectModule();
-    
+
     // - Read the current configuration of the IO direction
     wBuf[0] = MCP23008_IODIR;
     bcm2835_i2c_read_register_rs(wBuf,rBuf,1);
     io_dir_mask = rBuf[0];
-    
+
     if(_pin > 7 ){
         printf("Error assigning the IO direction in the Digital IO Device...\n");
         return;
@@ -73,18 +73,18 @@ void MCP23008::setDir(uint8_t _pin, uint8_t dir){
 void MCP23008::setDir(uint8_t* pin_array, uint8_t dir, uint8_t pin_count){
     uint8_t io_dir_mask;
     selectModule();
-    
+
     // - Read the current configuration of the IO direction
     wBuf[0] = MCP23008_IODIR;
     bcm2835_i2c_read_register_rs(wBuf,rBuf,1);
     io_dir_mask = rBuf[0];
-    
+
     for(uint8_t i = 0; i < pin_count; i++){
         if(pin_array[i] > 7 ){
             printf("Error assigning the IO direction in the Digital IO Device...\n");
             return;
         }
-        
+
         if(dir == MCP23008_INPUT){
             io_dir_mask |= 1 << pin_array[i];
         }else{
@@ -98,18 +98,18 @@ void MCP23008::setDir(uint8_t* pin_array, uint8_t dir, uint8_t pin_count){
 void MCP23008::setPullUp(uint8_t* pin_array, uint8_t pu, uint8_t pin_count){
     uint8_t pu_mask;
     selectModule();
-    
+
     // - Read the current configuration of the IO direction
     wBuf[0] = MCP23008_GPPU;
     bcm2835_i2c_read_register_rs(wBuf,rBuf,1);
     pu_mask = rBuf[0];
-    
+
     for(uint8_t i = 0; i < pin_count; i++){
         if(pin_array[i] > 7 ){
             printf("Error setting pull-up resistor in the Digital IO Device...\n");
             return;
         }
-        
+
         if(pu == MCP23008_PULL_UP_EN){
             pu_mask |= 1 << pin_array[i];
         }else{
@@ -117,7 +117,7 @@ void MCP23008::setPullUp(uint8_t* pin_array, uint8_t pu, uint8_t pin_count){
         }
     }
     wBuf[1] = pu_mask;
-    bcm2835_i2c_write(wBuf,2);    
+    bcm2835_i2c_write(wBuf,2);
 }
 
 uint8_t MCP23008::readGPIO(){
@@ -141,17 +141,17 @@ void MCP23008::digitalWrite(uint8_t pin, uint8_t val){
         printf("Error writing the IO status from the Digital IO Device...\n");
         return;
     }
-    
+
     // - Read the current configuration of the IO direction
     io_mask = readGPIO();
-    
+
     if(val == MCP23008_HIGH){
         io_mask |= 1 << pin;
     }else{
         io_mask &= ~(1 << pin);
     }
 
-    writeGPIO(io_mask);    
+    writeGPIO(io_mask);
 }
 
 uint8_t MCP23008::digitalRead(uint8_t pin){
@@ -161,7 +161,7 @@ uint8_t MCP23008::digitalRead(uint8_t pin){
         printf("Error reading the IO status from the Digital IO Device...\n");
         return 0x00;
     }
-    
+
     // - Read the current configuration of the IO direction
     io_mask = readGPIO();
     return (io_mask >> pin) & 0x01;
@@ -169,42 +169,42 @@ uint8_t MCP23008::digitalRead(uint8_t pin){
 
 void MCP23008::writeMultiGPIO(uint8_t* pin_array, uint8_t *pin_data, uint8_t pin_count){
     uint8_t io_mask;
-    
+
     // - Read the current configuration of the IO direction
     io_mask = readGPIO();
-    
+
     for(uint8_t i = 0; i < pin_count; i++){
         if(pin_array[i] > 7 ){
             printf("Error writing IO output to the Digital IO Device...\n");
             return;
         }
-        
+
         if(pin_data[i] == MCP23008_HIGH){
             io_mask |= 1 << pin_array[i];
         }else{
             io_mask &= ~(1 << pin_array[i]);
         }
     }
-    writeGPIO(io_mask);    
+    writeGPIO(io_mask);
 }
 
 void MCP23008::writeMultiGPIO(uint8_t* pin_array, uint8_t pin_data, uint8_t pin_count){
     uint8_t io_mask;
-    
+
     // - Read the current configuration of the IO direction
     io_mask = readGPIO();
-    
+
     for(uint8_t i = 0; i < pin_count; i++){
         if(pin_array[i] > 7 ){
             printf("Error writing IO output to the Digital IO Device...\n");
             return;
         }
-        
+
         io_mask &= ~(1 << pin_array[i]); // - Clear current bit
         // Two steps: Obtain the value of the current bit, and set such value to the current bit
         io_mask |= ((pin_data >> pin_array[i]) & 0x01) << pin_array[i];
     }
-    writeGPIO(io_mask);    
+    writeGPIO(io_mask);
 }
 
 void MCP23008::bcm_init(){
@@ -216,7 +216,7 @@ void MCP23008::bcm_init(){
         printf("BCM2835 Error!!...\n");
         exit(1);
     }
-    
+
     bcm2835_i2c_begin();
 
     bcm2835_i2c_setClockDivider(clk_div);
@@ -224,9 +224,9 @@ void MCP23008::bcm_init(){
 
 void MCP23008::bcm_end(){
     bcm2835_i2c_end();
-    bcm2835_close();    
+    bcm2835_close();
 }
 
 void MCP23008::release(){
-    bcm_end();    
+    // bcm_end();    
 }
--- src/Modules/LCD/LCDModule.cpp
+++ src/Modules/LCD/LCDModule.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   LCDModule.cpp
  * Author: yova
- * 
+ *
  * Created on 28 de mayo de 2016, 02:38 PM
  */
 
@@ -17,11 +17,11 @@
 LCDModule::LCDModule(uint8_t _addr) {
     if(_addr > 3){ // - 2 bits for custom address
         printf("Wrong slave address for the LCD Module...\n");
-        return;    
+        return;
     }
 
     mcp = new MCP23008(LCD_ADDRESS | _addr);
-    
+
     _displayfunction = LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS;
 
     // the I/O expander pinout
@@ -51,20 +51,20 @@ LCDModule::~LCDModule() {
 void LCDModule::initializeLCD(){
     // - Set all the putput pins
     mcp->setDir(_data_pins,MCP23008_OUTPUT,8);
-    
+
     // - Enable the Backlight
     mcp->digitalWrite(_backlight_pin,MCP23008_HIGH);
 
     // - RW operation
     mcp->digitalWrite(_rw_pin,MCP23008_LOW);
-    
+
 //    if(lines > 1){
 //        _displayfunction |= LCD_2LINE;
 //    }
-    
+
     _numlines = 2;
     _currline = 0;
-    
+
     // - Initialization delay, which is not really necessary since the RPI takes
     // some time to start up, But to keep consistency with the general application
     // case, we will perform initialization as usual.
@@ -73,26 +73,26 @@ void LCDModule::initializeLCD(){
     _write_pins[0] = _rs_pin; _data_write[0] = MCP23008_LOW;
     _write_pins[1] = _enable_pin; _data_write[1] = MCP23008_LOW;
     mcp->writeMultiGPIO(_write_pins,_data_write,2);
-    
+
     // - Put the LCD into 4 bit mode
     writeData(0x03);
     uDelay(4120);
-    
+
     writeData(0x03);
     uDelay(120);
 
     writeData(0x03);
 //    uDelay(150);
-    
+
     writeData(0x02);
 //    uDelay(150);
 
     command(LCD_FUNCTIONSET | _displayfunction);
-    
-    // turn the display on with no cursor or blinking default    
-    _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;  
-    display();    
-    
+
+    // turn the display on with no cursor or blinking default
+    _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+    display();
+
     clear();
     // Initialize to default text direction (for romance languages)
     _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
@@ -102,41 +102,41 @@ void LCDModule::initializeLCD(){
 
 void LCDModule::writeData(uint8_t value){
     uint8_t io_mask;
-    
+
     // - Read the current GPIO status
     io_mask = mcp->readGPIO();
-    
+
     for(uint8_t i = 0; i < 4; i++){
         if(_data_pins[i] > 7 ){
             printf("Error writing IO output to the Digital IO Device...\n");
             return;
         }
-        
+
         io_mask &= ~(1 << _data_pins[i]); // - Clear current bit
         // Two steps: Obtain the value of the current bit, and set such value to the current bit
         io_mask |= ((value >> i) & 0x01) << _data_pins[i];
     }
-    
+
     // Make sure enable is LOW
     io_mask &= ~(1 << _enable_pin);
-    mcp->writeGPIO(io_mask);    
-    
+    mcp->writeGPIO(io_mask);
+
     uDelay(1);
-    
+
     io_mask |= (1 << _enable_pin); // Enable to High
     mcp->writeGPIO(io_mask);
-    
+
     uDelay(1);
-    
+
     io_mask &= ~(1 << _enable_pin); // Enable to LOW
-    mcp->writeGPIO(io_mask);    
-    
+    mcp->writeGPIO(io_mask);
+
     uDelay(1);
 }
 
 void LCDModule::send(uint8_t value, uint8_t mode){
     mcp->digitalWrite(_rs_pin,mode);
-    
+
     writeData((uint8_t)(value>>4));
     writeData(value);
 }
@@ -168,7 +168,7 @@ void LCDModule::printChar(char _char){
 
 void LCDModule::message(std::string _string){
     uint8_t length = 1;
-    for(char& c:_string){        
+    for(char& c:_string){
         if(length == 16 or c == '\n'){
             nextLine();
             if(length == 16){
@@ -180,20 +180,20 @@ void LCDModule::message(std::string _string){
             length++;
         }
     }
-    
+
 //    if(length > right_spaces){
 //        right_spaces = length;
 //    }
-//    
+//
 //    if(right_spaces > 40){
 //       right_spaces =  24;
 //    }else{
 //        if(right_spaces >= 16)
-//            right_spaces -= 16; 
+//            right_spaces -= 16;
 //        else
 //            right_spaces = 0;
 //    }
-//    
+//
 //    return right_spaces;
 }
 
@@ -209,7 +209,7 @@ void LCDModule::autoScroll(uint8_t right_spaces){
             home();
             counter = 0;
         }
-        counter++;        
+        counter++;
     }
 }
 
@@ -253,8 +253,8 @@ void LCDModule::release(){
     clear();
     setBacklight(0);
     noDisplay();
-    mcp->release();
-    delete mcp;    
-    
+    // mcp->release();
+    delete mcp;
+
     printf("[LCDModule] => Released\n");
 }
